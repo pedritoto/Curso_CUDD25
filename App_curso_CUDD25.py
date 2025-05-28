62272a387ffe8f344242d34ea36097239ac6c670
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-28 17:31:34 -0600

    .

--- App_curso_CUDD25.py
+++ App_curso_CUDD25.py
@@ -1,8 +1,9 @@
 import streamlit as st   
 st.title("Mi primer App")
+data='https://www.youtube.com/live/gdluMrQstCU?si=PTwieviPEJ82bh7y'
 st.video(data)
 bt=st.button("Da click")
-data='https://www.youtube.com/live/gdluMrQstCU?si=PTwieviPEJ82bh7y'
+
 if bt:
   st.image('par1.png')
 
