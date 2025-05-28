e873f185a2b6b3ac59c8987c5dfbb9457d6dcb93
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-28 17:46:06 -0600

    .

--- App_curso_CUDD25.py
+++ App_curso_CUDD25.py
@@ -13,3 +13,21 @@ elif option=='foto':
 
 
 #jjlksdjlkjsdlkfjsldkfjsdfsdfs
+
+# mini bot
+
+
+#st.set_page_config(page_title="Ejemplo Chat", layout="centered")
+
+st.title("💬 Mini Chatbot (solo repite lo que dices)")
+
+# Entrada tipo chat (abajo de la pantalla)
+user_input = st.chat_input("Escribe algo...")
+
+# Si el usuario escribe algo, mostramos los mensajes
+if user_input:
+    # Mostrar el mensaje del usuario
+    st.chat_message("user").write(user_input)
+
+    # Mostrar una respuesta simple del asistente
+    st.chat_message("assistant").write(f"{user_input} <- eso dijiste")
