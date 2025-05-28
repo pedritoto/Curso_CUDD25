7ccb28eb76d84f97b4125551546f12eeeefb0d12
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-28 17:27:43 -0600

    .

--- App_curso_CUDD25.py
+++ App_curso_CUDD25.py
@@ -5,5 +5,5 @@ data='https://www.youtube.com/live/gdluMrQstCU?si=PTwieviPEJ82bh7y'
 if bt:
   st.image('par1.png')
 
-st.video(data, start_time=0, *, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+st.video(data, start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 #jjlksdjlkjsdlkfjsldkfjsdfsdfs
