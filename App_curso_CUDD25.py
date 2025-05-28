3a2894eca7be3b6f032be6ad375dbb21769d561c
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-28 17:33:35 -0600

    .

--- App_curso_CUDD25.py
+++ App_curso_CUDD25.py
@@ -1,6 +1,6 @@
 import streamlit as st   
 st.title("Mi primer App")
-data='https://youtu.be/51bKJCa8h-0?si=FP0dHh62dVm9yCkZ'
+data='https://youtu.be/FFPXm-tuOt8?si=FN9EdwXnuF-PGHHx'
 st.video(data)
 bt=st.button("Da click")
 
