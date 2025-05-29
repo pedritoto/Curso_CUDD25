701d6186adf55c97be09b789c5c1d313eabbc96d
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-29 15:52:10 -0600

    .

--- App16_cudd25.py
+++ App16_cudd25.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 
-openai_api_key = st.secrets["api_key"] 
+#openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
-client = OpenAI(api_key=openai_api_key)
+#client = OpenAI(api_key=openai_api_key)
 
 #prompt = st.chat_input("What is up?")
 #if prompt==None:
