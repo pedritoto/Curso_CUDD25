525ae4ad7ff1844fe6d3d7dc3881ff5a072e957e
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-29 16:16:54 -0600

    .

--- App16_cudd25.py
+++ App16_cudd25.py
@@ -12,7 +12,7 @@ prompt = st.chat_input("What is up?")
 if prompt==None:
    st.stop()
 
-with st.chat_message("user",avatar="😾 "):
+with st.chat_message("user",avatar="😾"):
    st.markdown(prompt)
 
 # Generate a response using the OpenAI API.
