27b384f2d5a1d66c720245e64c0e303cfdcea6e7
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-28 18:14:47 -0600

    Create App16_cudd25.py

---
+++ App16_cudd25.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("💬 Chatbot")
+
+openai_api_key = st.secrets["api_key"] 
+# Create an OpenAI client.
+client = OpenAI(api_key=openai_api_key)
+
+#prompt = st.chat_input("What is up?")
+#if prompt==None:
+#   st.stop()
+
+#with st.chat_message("user"):
+#   st.markdown(prompt)
+
+# Generate a response using the OpenAI API.
+
+#stream = client.chat.completions.create(
+#        model="gpt-4o-mini",  
+#        messages=[
+#            {"role": "system", "content": "You are an assistant."},
+#            {"role": "user", "content": prompt}
+#        ],
+#        max_tokens=800,
+#        temperature=0,
+#    )
+#respuesta = stream.choices[0].message.content
+
+#with st.chat_message("assistant"):
+#   st.write(respuesta)
