83d8d77495997da3ad7c14f4c5ab1e2d03e232d5
pedritoto <84814111+pedritoto@users.noreply.github.com>
2025-05-29 16:06:45 -0600

    .

--- App16_cudd25.py
+++ App16_cudd25.py
@@ -1,12 +1,12 @@
 import streamlit as st
-#from openai import OpenAI
+from openai import OpenAI
 
 # Show title and description.
 #st.title("💬 Chatbot")
 
-#openai_api_key = st.secrets["api_key"] 
+openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
-#client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key=openai_api_key)
 
 #prompt = st.chat_input("What is up?")
 #if prompt==None:
